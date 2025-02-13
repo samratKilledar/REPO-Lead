4b77b95ebcad84a9ec1c2b314f0476ba32a48a0a
SamratKilledar <samratkilledar@gmail.com>
2025-02-13 14:08:15 +0530

    addButton

--- src1/components/Button.js
+++ src1/components/Button.js
@@ -4,8 +4,8 @@ import ButtonStyles from '../styles/ButtonStyles';
 const Press = (props) => {
   return (
     <View>
-       <TouchableOpacity  style={[ButtonStyles.maintxt, props.customStyle]}>
-       <Text style={[ButtonStyles.buttonText, props.textStyles]}>{props.title}</Text>
+       <TouchableOpacity  style={[ButtonStyles.blueButton, props.customStyle]}>
+       <Text style={[ButtonStyles.blueButtonText, props.textStyles]}>{props.title}</Text>
        </TouchableOpacity>
        
     </View>
