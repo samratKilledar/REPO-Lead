85279aaf1ee7d4a6b3c8bf65a6d55738886c9629
Akhil054 <akhil.tamgaonkar@gmail.com>
2025-02-19 11:30:18 +0530

    Added packages updated app

--- App.tsx
+++ App.tsx
@@ -8,12 +8,36 @@
 import React from 'react';
 import {View,Text} from "react-native";
 import StartApp from "./src1/StartApp";
+import LeadAddServices from './src1/screens/Main/LeadAddServices';
+import LeadAddPersonal from './src1/screens/Main/LeadAddPersonal';
+import LeadAddOccupation from './src1/screens/Main/LeadAddOccupation';
+import LeadLast from './src1/screens/Main/LeadLast';
 const App=()=>{
   return(
     <View style={{flex:1}}>
-          <StartApp/>
-    </View>
+      {/* <StartApp/> */}
+      {/* <LeadAddServices/>  DONE    */}
+       {/* <LeadAddPersonal></LeadAddPersonal> remain the spaces  */}
+      {/* <LeadAddOccupation></LeadAddOccupation> require stepper improve  & Done  */}
+      {/* <LeadLast></LeadLast>   DONE  */}
+   </View>
   )
 }
 
 export default App;
+
+
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import LeadNavigation from './src1/navigation/LeadNavigation'// Adjust the path as needed
+// import AppNavigator from './src1/navigation/LeadNavigation';
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <AppNavigator/>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
