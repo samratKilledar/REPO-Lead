f45a1456ec2ade38f8d8eec2ba23262ba40dc386
Samrat Killedar <samratKilledar@example.com>
2025-02-22 16:09:38 +0530

    change

--- src1/styles/TextStyle.js
+++ src1/styles/TextStyle.js
@@ -21,54 +21,7 @@ export default StyleSheet.create({
         marginTop: 20,
     },
 
-    forgotPasswordLogin: {
-        fontFamily: "Urbanist",
-        fontWeight: "600",
-        fontSize: 16,
-        lineHeight: 22.4,
-        letterSpacing: 0.2,
-        textAlign: "center",
-        color: "#2B2162", // ✅ Correct color
-        marginTop: 20,
-    },
 
-
-    subheading: {
-        fontfamily: "Urbanist",
-        fontWeight: 400,
-        fontSize: 16,
-        lineHeight: 22.4,
-    },
-
-    maintxt: {
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 48,
-        lineHeight: 52.8,
-    },
-
-    maintext_1: {
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 40,
-        lineHeight: 44,
-    },
-
-    buttontypo: {
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 16,
-        lineHeight: 22.4,
-
-    },
-
-    buttonsubtxt: {
-        fontfamily: "Urbanist",
-        fontWeight: 600,
-        fontSize: 16,
-        lineHeight: 22.4,
-
-    },
 
     //Forgot and Reset Password 
     Titles: {                   //H4 bold, Taska 
@@ -143,74 +96,8 @@ export default StyleSheet.create({
         lineHeight: 21.6,
     },
 
-
-    card_Titles: {              //Tiki Mobile app  + Nav Titles + also the part of 05(Create & Edit ) + 06 + Profile Titles 
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 24,
-        lineHeight: 28.8,
-
-    },
-
-    card_sub_tilte: {               //UI kit Design 
-        fontfamily: "Urbanist",
-        fontWeight: 400,
-        fontSize: 14,
-        lineHeight: 19.6,
-    },
-
-    card_chip: {                  //80/90 + search bar 
-        fontfamily: "Urbanist",
-        fontWeight: 600,
-        fontSize: 14,
-        lineHeight: 19.6,
-    },
-
-    card_chips: {
-        fontfamily: "Urbanist",
-        fontWeight: 400,
-        fontSize: 12,
-        lineHeight: 14.4,
-    },
-
-    //After task & subTitle prop is use for task title 
-
-    Task_Title: {              // i.e Today's Task + sub Task Titles 
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 20,
-        lineHeight: 24,
-    },
-
-    Task_descp: {
-        fontfamily: "Urbanist",
-        fontWeight: 400,
-        fontSize: 14,
-        lineHeight: 19.6,
-    },
-
-    //05 Create & Edit 
-    CE_Typo: {                    // E-wallet part + 6 Inbox Section Call & Chat 
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 32,
-        lineHeight: 35.2,
-    },
-
-    CE_button_ty: {
-        fontfamily: "Urbanist",
-        fontWeight: 700,
-        fontSize: 18,
-        lineHeight: 25.2,
-    },
-
+   
     //06 Profile & Setting  yet name to decide 
-    other_info: {
-        fontfamily: "Urbanist",
-        fontWeight: 600,
-        fontSize: 18,
-        lineHeight: 25.2,
-    },
         //Home page
         textNum: {
             fontSize: 24,
