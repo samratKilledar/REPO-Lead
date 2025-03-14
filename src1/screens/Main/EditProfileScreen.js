c81cc5eda247f273a3f41dc892ad4b1c5f9f7bb9
SamruddhiDesai <samruddhi.desai9603@gmail.com>
2025-03-14 10:40:01 +0530

    lazy load

--- src1/screens/Main/EditProfileScreen.js
+++ src1/screens/Main/EditProfileScreen.js
@@ -1,9 +1,8 @@
-
-import React, { useState } from "react";
-import { View, StyleSheet, Image, TextInput, Platform, ScrollView, Alert } from "react-native";
-import Dropdown from "../../components/Dropdown";
-import CustomTextInput from "../../components/CustomTextInput";
-import CustomButton from "../../components/CustomButton";
+import React, { useState, lazy, Suspense } from "react";
+import { View, StyleSheet, Image, TextInput, Platform, ScrollView, Alert, ActivityIndicator } from "react-native";
+// import Dropdown from "../../components/Dropdown";
+// import CustomTextInput from "../../components/CustomTextInput";
+// import CustomButton from "../../components/CustomButton";
 import ButtonStyles from "../../styles/ButtonStyles";
 import NavigationHeaderBack from "../../components/NavigationHeaderBack";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -16,12 +15,16 @@ import {
     updatePhoneNumber, 
     updateGender,
     updateDate,
-    editProfileUser,
 } from "../../redux/actions/editProfileActions";
+
+// Lazy Load Components
+const Dropdown = lazy(() => import("../../components/Dropdown"));
+const CustomTextInput = lazy(() => import("../../components/CustomTextInput"));
+const CustomButton = lazy(() => import("../../components/CustomButton"));
+
 
 const EditProfileScreen = (props) => {
     const dispatch = useDispatch();
-    
     const firstname = useSelector(state => state.editProfile.firstname);
     const lastname = useSelector(state => state.editProfile.lastname);
     const email = useSelector(state => state.editProfile.email);
@@ -96,21 +99,26 @@ const EditProfileScreen = (props) => {
         }
 
         Alert.alert("Success", "Profile updated successfully!");
-        
+
     };
 
     return (
         <View style={styles.container}>
             <View style={{ flex: 0.1, marginLeft: 5 }}>
-                <NavigationHeaderBack text="Edit Profile" onPress={goBackCall} />
+                <NavigationHeaderBack text="Edit Profile" onPress={goBackCall} />   
             </View>
 
             <ScrollView style={styles.container1} showsVerticalScrollIndicator={false}>
                 <View style={styles.centerContainer}>
+                    <Suspense fallback={<CustomTextInput/>}>
                     <CustomTextInput value={firstname} placeholder={firstnamePlaceholder} onChangeText={(text) => dispatch(updateFirstname(text))} />
+                    </Suspense>
+                    <Suspense fallback={<CustomTextInput/>}>
                     <CustomTextInput value={lastname} placeholder={lastnamePlaceholder} onChangeText={(text) => dispatch(updateLastname(text))} />
+                    </Suspense>
+                    <Suspense fallback={<CustomTextInput/>}>
                     <CustomTextInput followupicon={require('../../assets/icons/Message.png')} value={email} placeholder={emailPlaceholder} onChangeText={(text) => dispatch(updateEmail(text))} />
-
+                    </Suspense>
                     <View style={styles.phoneInputContainer}>
                         <Image source={require('../../assets/icons/Country.png')} style={styles.flagIcon} />
                         <Image source={require('../../assets/icons/arrowDownblack.png')} style={styles.dropdownIcon} />
@@ -123,6 +131,7 @@ const EditProfileScreen = (props) => {
                         />
                     </View>
 
+                    <Suspense fallback={<Dropdown/>}>
                     <Dropdown
                         label={genderPlaceholder}
                         selectedValue={gender}
@@ -134,7 +143,8 @@ const EditProfileScreen = (props) => {
                         ]}
                         zIndex={2000}
                     />
-
+                    </Suspense>
+                    <Suspense fallback={<CustomTextInput/>}>
                     <CustomTextInput
                         followupicon={require('../../assets/icons/Calendar/calendar.png')}
                         value={date}
@@ -149,6 +159,8 @@ const EditProfileScreen = (props) => {
                             onChange={handleDateChange}
                         />
                     )}
+                    </Suspense>
+                    <Suspense fallback={<CustomTextInput/>}>
                     <CustomTextInput
                         followupicon={require('../../assets/icons/Scan/scan.png')}
                         type={attachment} 
@@ -156,12 +168,15 @@ const EditProfileScreen = (props) => {
                         placeholder="Profile Photo"
                         onIconPress={handleImagePick}
                     />
+                    </Suspense>
+                    <Suspense fallback={<CustomButton/>}>
                     <CustomButton
                         title="Update"
                         customStyle={ButtonStyles.blueButton} 
                         textStyles={ButtonStyles.blueButtonText} 
                         onPress={handleUpdateProfile} 
                     />
+                    </Suspense>
                 </View>
             </ScrollView>
         </View>
@@ -221,11 +236,6 @@ const styles = StyleSheet.create({
 });
 
 export default EditProfileScreen;
-
-
-
-
-
 
 
 
